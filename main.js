57a1c63195a483f53aedb656721331d788fed085
kayoJr <kalebmelaku97@gmail.com>
2022-04-27 20:00:52 +0300

    Desktop version only

---
+++ main.js
@@ -0,0 +1,13 @@
+// var x = document.querySelectorAll(".test");
+// var y = document.querySelectorAll(".icon");
+
+
+
+// x.forEach(function(r) {
+//     r.addEventListener('mouseover', function(){
+//         console.log("link");
+//         y.forEach(function(q) {
+//             if(event.target co)
+//             })
+//         })
+//     })
